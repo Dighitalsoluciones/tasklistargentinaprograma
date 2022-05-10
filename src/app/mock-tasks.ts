137e4b22e2bab6b5a74227c58a59da5eb2582ea0
Dighitalsoluciones <hernanglomba@gmail.com>
2022-05-10 13:50:32 -0300

    se agrego el mock de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+import {Task} from "./Task"
+
+export const TASK: Task[] = [
+{
+  id: 1,
+  text: "Terminar primer modulo de Angular",
+  day: 'Agosto 5 a las 12 hs',
+  reminder: true
+},
+{
+    id: 2,
+    text: "Ir a tomar mate",
+    day: 'Agosto 5 a las 17 hs',
+    reminder: true
+},
+{
+    id: 3,
+    text: "Investigar sobre Bootstrap",
+    day: 'Agosto 5 a las 19 hs',
+    reminder: false
+},
+
+]
